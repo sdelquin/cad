7d76a3a5a8b892ba1bb7ca1d01d43c368c038f8a
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-08-21 14:09:09 +0100

    Add generation of main page

---
+++ cad.py
@@ -0,0 +1,39 @@
+import os
+import re
+import shlex
+import subprocess
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
+
+
+def pdf2png(source_path, target_path):
+    cmd = f'convert {source_path} {target_path}'
+    subprocess.run(shlex.split(cmd))
+
+
+def build_design_options(force_rewrite=False):
+    desing_options = []
+    for dirpath, _, filenames in os.walk('designs'):
+        if 'render.png' in filenames:
+            if 'drawing.pdf' not in filenames:
+                print(f'drawing.pdf not found at {dirpath}')
+            else:
+                if force_rewrite or 'drawing.png' not in filenames:
+                    drawing_pdf = os.path.join(dirpath, 'drawing.pdf')
+                    drawing_png = os.path.join(dirpath, 'drawing.png')
+                    pdf2png(drawing_pdf, drawing_png)
+            cleanpath = re.sub(r'^designs/?', '', dirpath)
+            option = f'<option>{cleanpath}</option>'
+            desing_options.append(option)
+    return desing_options
+
+
+def build():
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('index.tmpl.html')
+    p = Path('index.html')
+    p.write_text(template.render(designs=build_design_options(True)))
+
+
+build()
