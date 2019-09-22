aeb2e4e98030049c1e0f25767ebe3b657852a158
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-09-22 20:38:44 +0100

    Add colors to console logs

--- cad.py
+++ cad.py
@@ -1,21 +1,16 @@
-import logging
 import os
 import re
 import shlex
 import subprocess
 from pathlib import Path
 
-import coloredlogs
+import colorful
 from jinja2 import Environment, FileSystemLoader
-
-
-logger = logging.getLogger(__name__)
-coloredlogs.install(level='DEBUG')
 
 
 def pdf2png(source_path, target_path):
     cmd = f'convert {source_path} {target_path}'
-    logger.info(cmd)
+    print(colorful.italic_cyan(f'  {cmd}'))
     subprocess.run(shlex.split(cmd), stderr=subprocess.DEVNULL)
 
 
@@ -23,14 +18,18 @@ def build_design_options(force_rewrite=False):
     desing_options = []
     for dirpath, _, filenames in os.walk('designs'):
         if 'render.png' in filenames:
-            logger.info(f'Building design at {dirpath}')
+            print(f'Scanning design at {dirpath}')
             if 'drawing.pdf' not in filenames:
-                logger.error(f'drawing.pdf not found at {dirpath}')
+                print(
+                    colorful.bold_coral(
+                        f'  drawing.pdf not found at {dirpath}'))
             else:
                 if force_rewrite or 'drawing.png' not in filenames:
                     drawing_pdf = os.path.join(dirpath, 'drawing.pdf')
                     drawing_png = os.path.join(dirpath, 'drawing.png')
                     pdf2png(drawing_pdf, drawing_png)
+                else:
+                    print(colorful.lavender('  Nothing to do!'))
             cleanpath = re.sub(r'^designs/?', '', dirpath)
             option = f'<option>{cleanpath}</option>'
             desing_options.append(option)
@@ -41,7 +40,8 @@ def build():
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template('index.tmpl.html')
     p = Path('index.html')
-    p.write_text(template.render(designs=build_design_options(True)))
+    p.write_text(template.render(designs=build_design_options()))
 
 
-build()
+if __name__ == '__main__':
+    build()
